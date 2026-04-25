5982841a32c0f0a670338c6639266a443e70bfc9
Kannan Rajendran <kannannglabs@gmail.com>
2026-04-25 10:29:07 +0530

    complete the observability

---
+++ section11/accounts/src/main/java/com/eazybytes/controller/CustomerController.java
@@ -0,0 +1,35 @@
+package com.eazybytes.controller;
+
+import com.eazybytes.dto.CustomerDetailsDto;
+import com.eazybytes.service.ICustomerService;
+import jakarta.validation.constraints.Pattern;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(path = "/api/v1", produces = "application/json")
+@Validated
+public class CustomerController {
+
+    private static final Logger logger = LoggerFactory.getLogger(CustomerController.class);
+
+    private final ICustomerService customerService;
+    public CustomerController(ICustomerService customerService) {
+        this.customerService = customerService;
+    }
+    @GetMapping(value = "/fetchCustomerDetails")
+    public ResponseEntity<CustomerDetailsDto> fetchCustomerDetails(@RequestHeader("eazybank-correlation-id") String correlationId,
+                                                                   @RequestParam
+                                                                   @Pattern(regexp="(^$|[0-9]{10})",message = "Mobile number must be 10 digits")
+                                                                   String mobileNumber) {
+        //logger.debug("eazybank-correlation-Id found: {}", correlationId);
+        logger.debug("fetchCustomerDetails() method started.");
+        CustomerDetailsDto customerDetailsDto = customerService.fetchCustomerDetails(mobileNumber, correlationId);
+        logger.debug("fetchCustomerDetails() method ended.");
+        return ResponseEntity.status(HttpStatus.OK).body(customerDetailsDto);
+    }
+}
